fbb15e95d7d561b3381515b53827c42fe526b02f
Vovuk <109819093+Vovuk@users.noreply.github.com>
2022-08-05 22:10:38 +0300

    square number exam1

---
+++ exam1/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.WriteLine("Input square number: ");
+int sqnumber = int.Parse(Console.ReadLine());
+Console.WriteLine("Input number: ");
+int number = int.Parse(Console.ReadLine());
+if (sqnumber == number * number)
+{
+    Console.WriteLine("It's true");
+}
+else
+{
+    Console.WriteLine("It's false");
+}
