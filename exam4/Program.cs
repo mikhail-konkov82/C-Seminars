7035111619dbae675bd47ddb665584a35e7cf4d8
Vovuk <109819093+Vovuk@users.noreply.github.com>
2022-08-12 20:51:25 +0300

    functions

---
+++ exam4/Program.cs
@@ -0,0 +1,25 @@
+﻿// int a = 4;
+// double sqrtA = Math.Sqrt(a); // корень квадратный
+// Console.WriteLine(sqrtA);
+// int b = 2;
+// double powB = Math.Pow(b, 2); // возведение в степень
+// Console.WriteLine(powB);
+
+void PrintText(string text)
+{
+    if (string.IsNullOrEmpty(text))
+    {
+        Console.WriteLine("Empty String");
+        return;
+    }
+    Console.WriteLine(text);
+}
+
+int Sum(int x, int y)
+{
+    return x + y;
+}
+PrintText("");
+PrintText("Don't Empty string ");
+int result = Sum(5, 8);
+PrintText(result.ToString());
