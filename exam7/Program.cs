b0c0c020721c4309c6388e113b14ea888a6dfb2b
Vovuk <109819093+Vovuk@users.noreply.github.com>
2022-08-19 21:10:27 +0300

    summ pos and neg

---
+++ exam7/Program.cs
@@ -0,0 +1,51 @@
+﻿// Задача 1: Задайте массив из 10 элементов, заполненный случайными 
+// числами из промежутка [-9, 9]. Найдите сумму отрицательных и положительных элементов массива.
+
+// Например, в массиве [3,9,-8,1,0,-7,2,-1,8,-3,-1,6] сумма положительных 
+// чисел равна 29, сумма отрицательных равна -20
+
+
+
+
+int[] FillArray(int[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        array[i] = new Random().Next(-9, 10);
+    }
+    return array;
+}
+
+void PrintArray(int[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write(array[i] + " ");
+    }
+}
+
+void Summ(int[] array)
+{
+    int sumPos = 0;
+    int sumNeg = 0;
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (array[i] < 0)
+        {
+            sumNeg += array[i];
+        }
+        if (array[i] > 0)
+        {
+            sumPos += array[i];
+        }
+    }
+    Console.WriteLine(" ");
+    Console.WriteLine("Summ Positiv number: " + sumPos);
+    Console.WriteLine("Sum Negative number: " + sumNeg);
+}
+
+int[] array = new int[10];
+FillArray(array);
+PrintArray(array);
+Summ(array);
+
