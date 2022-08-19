6978c8401c8c18818ffa2479a46b100e3d5284fd
Vovuk <109819093+Vovuk@users.noreply.github.com>
2022-08-19 21:11:03 +0300

    recurse number

---
+++ exam8/Program.cs
@@ -0,0 +1,30 @@
+﻿// Задача 2: Задайте массив из 10 элементов и положительные элементы 
+// замените на соответствующие отрицательные, и наоборот.
+
+// [-4, -8, 8, 2] -> [4, 8, -8, -2]
+
+int[] FillArray(int[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        array[i] = new Random().Next(-9, 10);
+    }
+    Console.WriteLine(string.Join(", ", array));
+    return array;
+}
+
+
+int [] recurse(int[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        array[i] = 0 - array[i];
+    }
+    Console.WriteLine(string.Join(", ", array));
+    return array;
+}
+
+
+int[] array = new int[10];
+FillArray(array);
+recurse(array);
