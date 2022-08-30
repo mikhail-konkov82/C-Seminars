27ccf6b3bb09607d22c5f7e22971b8a141a9172c
Vovuk <109819093+Vovuk@users.noreply.github.com>
2022-08-30 10:01:50 +0300

    sqr even number in array

---
+++ exam13/Program.cs
@@ -0,0 +1,68 @@
+﻿// Задача 2: Задайте двумерный массив. Найдите элементы, 
+// у которых оба индекса чётные, и замените эти элементы на их квадраты.
+
+// Например, изначально массив выглядел вот так:
+
+// 1 4 7 2
+
+// 5 9 2 3
+
+// 8 4 2 4
+
+// Новый массив будет выглядеть вот так:
+
+// 1 4 7 2
+
+// 5 81 2 9
+
+// 8 4 2 4
+
+void PrintArray(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            Console.Write($"{array[i, j]} ");
+        }
+        Console.WriteLine();
+    }
+}
+
+void FillArray(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            array[i, j] = new Random().Next(0, 10);
+        }
+    }
+}
+
+void sqrEvenNumberArray(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            if (i % 2 == 0 && j % 2 == 0)
+            {
+                array[i, j] = array[i, j] * array[i, j];
+            }
+        }
+    }
+
+}
+
+Console.WriteLine("Input size array");
+Console.Write("Input number of rows: ");
+int n = int.Parse(Console.ReadLine());
+Console.Write("Input number of columns: ");
+int m = int.Parse(Console.ReadLine());
+int[,] array = new int[n, m];
+FillArray(array);
+PrintArray(array);
+Console.WriteLine("------------------------------------------------------");
+sqrEvenNumberArray(array);
+PrintArray(array);
